aa4499c7f148b000f5810a14e58d70d57357bd73
Fr4nks <fsdevilliers@gmail.com>
2023-04-28 21:34:21 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,28 @@
 input.onButtonPressed(Button.A, function () {
-    ball.change(LedSpriteProperty.X, -1)
+    ship.change(LedSpriteProperty.X, -1)
 })
 input.onButtonPressed(Button.B, function () {
-    ball.change(LedSpriteProperty.X, 1)
+    ship.change(LedSpriteProperty.X, 1)
 })
-let ball: game.LedSprite = null
+let ship: game.LedSprite = null
 game.setLife(6)
-ball = game.createSprite(2, 4)
-ball.set(LedSpriteProperty.Brightness, 10)
-let hole = game.createSprite(randint(0, 4), 0)
+ship = game.createSprite(2, 4)
+ship.set(LedSpriteProperty.Brightness, 10)
+let meteor = game.createSprite(randint(0, 4), 0)
 while (true) {
     basic.pause(300)
-    hole.change(LedSpriteProperty.Y, 1)
-    if (ball.isTouching(hole)) {
-        hole.delete()
+    meteor.change(LedSpriteProperty.Y, 1)
+    if (ship.isTouching(meteor)) {
+        meteor.delete()
         music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 1600, 4610, 255, 0, 50, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
         game.addScore(1)
-    } else if (hole.get(LedSpriteProperty.Y) == 4) {
-        basic.pause(300)
-        hole.delete()
+    } else if (meteor.get(LedSpriteProperty.Y) == 4) {
+        basic.pause(100)
+        meteor.delete()
         music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 200, 600, 255, 0, 15, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
         game.removeLife(1)
     }
-    if (hole.isDeleted()) {
-        hole = game.createSprite(randint(0, 4), 0)
+    if (meteor.isDeleted()) {
+        meteor = game.createSprite(randint(0, 4), 0)
     }
 }
